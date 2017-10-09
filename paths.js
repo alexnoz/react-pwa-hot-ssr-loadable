d7dfda833970d8947e600d76aed4a05965011af6
Alex Noz <alex-nozzz@mail.ru>
2017-10-09 19:48:09 +0900

    Remove extraneous paths

--- paths.js
+++ paths.js
@@ -6,15 +6,12 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 
 module.exports = {
   app: resolveApp('app'),
-  images: resolveApp('app/images'),
-  fonts: resolveApp('app/fonts'),
   components: resolveApp('app/components'),
   appIndex: resolveApp('app/index.js'),
   public: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   manifest: resolveApp('public/manifest.json'),
   favicon: resolveApp('public/favicon.ico'),
-  pwaIcons: resolveApp('public/icons'),
   build: resolveApp('build'),
 
   // These paths are relatve to the 'build' folder
