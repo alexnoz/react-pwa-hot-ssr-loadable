adf45396f783addcffd007c834fccd240159012c
Alex Nozdriukhin <alex-nozzz@mail.ru>
2017-10-30 18:08:01 +0900

    Add a lazy-loaded component example

--- app/components/about.js
+++ app/components/about.js
@@ -1,20 +1,41 @@
 import React from 'react'
 
-const About = () => (
-  <div>
-    <h2>About:</h2>
-    <p>The aim of this project is to provide a boilerplate that helps to build a PWA using SSR, HMR and Code Splitting</p>
-    <h3>Features:</h3>
-    <ul>
-      <li>Hot module replacement</li>
-      <li>Server-side rendering</li>
-      <li>Code splitting</li>
-      <li>Progressive Web App (passes all of the {
-        <a rel='noopener noreferer' target='_blank' href='https://developers.google.com/web/tools/lighthouse/'>lighthouse's</a>
-      } audits)
-      </li>
-    </ul>
-  </div>
-)
+// use this function to do component-level code splitting
+import loadComponent from '../routes'
+
+const Login = loadComponent('Login', 'login')
+
+class About extends React.Component {
+  state = { showLogin: false }
+
+  onClick = e => {
+    this.setState(prevState => ({
+      showLogin: !prevState.showLogin
+    }))
+  }
+
+  render () {
+    const { showLogin } = this.state
+
+    return (
+      <div>
+        <h2>About:</h2>
+        <p>The aim of this project is to provide a boilerplate that helps to build a PWA using SSR, HMR and Code Splitting</p>
+        <h3>Features:</h3>
+        <ul>
+          <li>Hot module replacement</li>
+          <li>Server-side rendering</li>
+          <li>Code splitting</li>
+          <li>Progressive Web App (passes all of the {
+            <a rel='noopener noreferer' target='_blank' href='https://developers.google.com/web/tools/lighthouse/'>lighthouse's</a>
+          } audits)
+          </li>
+        </ul>
+        <button onClick={this.onClick}>{showLogin ? 'Hide' : 'Show'} a dynamic component example</button>
+        {showLogin && <Login />}
+      </div>
+    )
+  }
+}
 
 export default About
---
+++ app/components/login/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './login.scss'
+
+const Login = () => (
+  <div styleName='wrapper'>
+    <h2 styleName='heading'>Hey, I'am a dynamically loaded form</h2>
+    <form action='/' styleName='form'>
+      <label htmlFor='name' styleName='label'>Name</label>
+      <input id='name' type='text' styleName='input' name='name' placeholder='Your name...' />
+      <label htmlFor='email' styleName='label'>Email</label>
+      <input id='email' type='email' styleName='input' name='name' placeholder='Your email...' />
+      <button type='submit' styleName='submit'>Submit</button>
+    </form>
+  </div>
+)
+
+export default Login
